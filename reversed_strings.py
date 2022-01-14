a65c0b545d6731ebf1aad9899e370f137c545964
Emma Samuel <emmameaden95@gmail.com>
2022-01-14 18:08:02 +0000

    Create reversed_strings.py

---
+++ reversed_strings.py
@@ -0,0 +1,9 @@
+#Complete the solution so that it reverses the string passed into it.
+
+def solution(string):
+    return string[::-1]
+
+print(solution('world'))
+print(solution('hello'))
+print(solution(''))
+print(solution('h'))
