f6d2165944d385e266a516972b4cea84c03de0e6
Emma Samuel <emmameaden95@gmail.com>
2022-01-22 22:54:24 +0000

    Create convert_a_number_to_a_string.py

---
+++ convert_a_number_to_a_string.py
@@ -0,0 +1,10 @@
+#We need a function that can transform a number into a string.
+
+def number_to_string(num):
+    return str(num)
+
+print(number_to_string(67))
+print(number_to_string(79585))
+print(number_to_string(79585))
+print(number_to_string(1+2))
+print(number_to_string(1-2))
